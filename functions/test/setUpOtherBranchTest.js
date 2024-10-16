32052d018a65b1f2d476f02f812792a498a0472e
Daniel Wilkins <danwilkins2003@gmail.com>
2024-10-16 12:49:18 +0100

    Full confidence works for other branch

--- functions/test/setUpOtherBranchTest.js
+++ functions/test/setUpOtherBranchTest.js
@@ -26,12 +26,11 @@ export default function setUpOtherBranchTest(
         const childNode = currentNode.children.find((child) => child.move === moveObj.move);
         const noOfChildren = currentNode.children.length;
 
-        
         if (moveNo % 2 === colorMoveNumber && noOfChildren === 1) {
             otherBranchMoveList.push({
                 move: moveObj.move,
                 skip: false,
-                confidence: (moveObj.confidence || 0),
+                confidence: moveObj.confidence || 0,
             });
         } else {
             otherBranchMoveList.push({ move: moveObj.move, skip: true });
@@ -43,12 +42,20 @@ export default function setUpOtherBranchTest(
     setMoveList(otherBranchMoveList);
 
     let index = null;
+    let backupIndex = null;
     // Find Move Index
     for (const moveObj of otherBranchMoveList) {
+        if (!moveObj.skip && backupIndex === null) {
+            backupIndex = otherBranchMoveList.indexOf(moveObj);
+        }
         if (!moveObj.skip && (moveObj.confidence || 0) < 2) {
             index = otherBranchMoveList.indexOf(moveObj);
             break;
         }
+    }
+
+    if (index === null) {
+        index = backupIndex;
     }
 
     chess.reset();
